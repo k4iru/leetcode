865d8ef07e47a41db0942c294116cba0ef178aea
Kyle Cheung <kcheu4@uwo.ca>
2022-04-30 17:33:07 -0400

    Update threeSum.js

--- two_pointer/threeSum.js
+++ two_pointer/threeSum.js
@@ -10,6 +10,20 @@ function threeSum(nums) {
 
     if (nums.length < 3) return [];
 
+    function twoSum(nums, k) {
+        let map = new Map();
+
+        for( let i = 0; i < nums.length; i++) {
+            let temp = k - nums[i];
+            if (map.has(temp)) {
+                return [map.get(temp), nums[i], k];
+            }
+            else {
+                map.set(nums[i], i);
+            }
+        }
+    }
+
 }
 
 module.exports = threeSum;
